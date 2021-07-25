302f5310b406b66fcd2b2127c9d9772285567cdc
SergiAndreaUwU <sergiosalazarordaz@gmail.com>
2021-07-25 13:40:27 -0500

    re-structured buttons for navigation

--- src/app/components/index/index.component.ts
+++ src/app/components/index/index.component.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-index',
-  templateUrl: './index.component.html',
-  styleUrls: ['./index.component.css']
+  selector: "app-index",
+  templateUrl: "./index.component.html",
+  styleUrls: ["./index.component.css"],
 })
 export class IndexComponent implements OnInit {
+  redFruits: Array<any> = [];
+  blueFruits: Array<any> = [];
+  greenFruits: Array<any> = [];
+  yellowFruits: Array<any> = [];
+  brownFruits: Array<any> = [];
 
-  constructor() { }
+  buttonList: Array<any> = [
+    { variant: "danger", category: "Red fruits", id: "red-fruit-button", bgImage: "assets/redfruits.jpg", fruitList: [{fruitName:"Apple",url:"#"}] },
+    { variant: "primary", category: "Blue fruits", id: "blue-fruit-button", bgImage: "assets/bluefruits.jpg", fruitList: [] },
+    { variant: "success", category: "Green fruits", id: "green-fruit-button", bgImage: "assets/greenfruits.jpg", fruitList: [] },
+    { variant: "warning", category: "Yellow fruits", id: "yellow-fruit-button", bgImage: "assets/yellowfruits.jpg", fruitList: [] },
+    { variant: "brown", category: "Brown fruits", id: "brown-fruit-button", bgImage: "assets/brownfruits.jpg", fruitList: [] },
+  ];
 
-  ngOnInit() {
-    
-  }
+  constructor() {}
 
+  ngOnInit() {}
 }
